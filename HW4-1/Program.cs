2440deb2a05f68e5dbcb8a3e578e3ae69b3739c7
PC <PC@DESKTOP-R4P1PJC>
2023-11-05 18:05:17 +0100

    Projektdateien hinzufügen.

---
+++ HW4-1/Program.cs
@@ -0,0 +1,57 @@
+﻿using System.Text;
+
+namespace Lesson
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+
+            double firstValue, secondValue;
+            string action;
+
+            Console.WriteLine("Enter number: 1");
+            firstValue = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter number: 2");
+            secondValue = double.Parse(Console.ReadLine());
+            Console.WriteLine("Choose the sign: '+' '-' '*' '/'");
+            action = Console.ReadLine();
+
+            switch (action)
+
+            {
+                case "+":
+                    Console.WriteLine(firstValue + secondValue);
+                    break;
+                case "-":
+                    Console.WriteLine(firstValue - secondValue);
+                    break;
+                case "*":
+                    Console.WriteLine(firstValue * secondValue);
+                    break;
+                case "/":
+                    
+                    if (secondValue == 0)
+                    {
+                        Console.WriteLine(0);
+                    }
+                    else
+                    {
+                        Console.WriteLine(firstValue / secondValue);
+                    }
+                    Console.WriteLine(firstValue / secondValue);
+                    break;
+                default:
+                    Console.WriteLine("Wrong!");
+                    break;
+
+
+            }
+            Console.ReadLine();
+
+
+        }
+    }
+}
